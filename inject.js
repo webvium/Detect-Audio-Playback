c4c5f431dc97fb0a6e905cd23f375c2a3d8ed734
Melvin Jones Repol <mrepol742@gmail.com>
2022-03-25 00:37:37 +0800

    Initial Commit

--- inject.js
+++ inject.js
@@ -3,3 +3,7 @@ const video = document.querySelector('video');
 video.addEventListener('play', (event) => {
     WebviumIdentifierUniqueGeneratedID.play();
 });
+
+video.addEventListener('pause', (event) => {
+    WebviumIdentifierUniqueGeneratedID.pause();
+});
